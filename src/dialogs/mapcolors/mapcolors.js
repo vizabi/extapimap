fbb2266caf373c83c8268f3fc7e4f27e4873079a
angie <konzeptmeister@gmail.com>
2023-04-04 17:20:38 +0200

    Remove unused legacy vizabi0 code

--- src/dialogs/mapcolors/mapcolors.js
+++ src/dialogs/mapcolors/mapcolors.js
@@ -54,31 +54,3 @@ export class Mapcolors extends Dialog {
 
 Dialog.add("mapcolors", Mapcolors);
 
-
-const _Mapcolors = {
-
-  /**
-   * Initializes the dialog component
-   * @param config component configuration
-   * @param parent component context (parent)
-   */
-  init(config, parent) {
-    this.name = "mapcolors";
-
-    this.components = [{
-      component: Vizabi.Component.get("indicatorpicker"),
-      placeholder: ".vzb-caxis-selector",
-      model: ["state.time", "state.marker.color_map", "locale"],
-      showHoverValues: true
-    }, {
-      component: Vizabi.Component.get("colorlegend"),
-      placeholder: ".vzb-clegend-container",
-      model: ["state.time", "state.entities", "state.marker", "state.marker.color_map", "locale", "ui"]
-    }];
-
-
-    this._super(config, parent);
-    this.template = require("./mapcolors.html");
-  }
-
-};
