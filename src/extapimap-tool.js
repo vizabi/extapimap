aef4cff9c23f83a133a0b4d15ca033081e00b68e
angie <konzeptmeister@gmail.com>
2024-08-30 21:13:29 +0200

    v4.3.2: Disallow weird scales on size

--- src/extapimap-tool.js
+++ src/extapimap-tool.js
@@ -123,7 +123,7 @@ ExtApiMap.DEFAULT_CORE = () => ({
     "size": {
       scale: {
         modelType: "size",
-        allowedTypes: ["linear", "log", "genericLog", "pow", "point"],
+        allowedTypes: ["linear", "point"],
       }
     },
     "size_label": {
