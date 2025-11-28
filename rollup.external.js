3240ba282a89fd8ad4dccb21c6d2b7899b9fc9e5
Angie Hjort <konzeptmeister@gmail.com>
2025-11-28 22:35:14 +0100

    v5.7.0: Don't bundle deck.gl into the project, use external file in page

--- rollup.external.js
+++ rollup.external.js
@@ -1,2 +1,6 @@
 module.exports = require("vizabi-tool-bundler")
-    .bind(null, 'ExtApiMap', 'extapimap', __dirname, require("./package.json"), {"mapbox-gl": "mapboxgl"});
+    .bind(null, 'ExtApiMap', 'extapimap', __dirname, require("./package.json"), {
+      "mapbox-gl": "mapboxgl",
+      "@deck.gl/core": "deck",
+      "@deck.gl/layers": "deck"
+    });
